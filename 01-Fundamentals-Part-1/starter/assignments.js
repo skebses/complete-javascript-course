31954e166f63ea49c75925049a8044f82ad9e1a0
Sebastian Stutz <sebastian.stutz99@gmail.com>
2021-10-26 10:27:58 +0200

    checkpoint chapter 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,37 @@
+// LECTURE: Values and Variables
+// let country = "Germany";
+// let continent = "Europe";
+// let population = 82;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// LECTURE: Data Types
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland)
+// console.log(typeof population)
+// console.log(typeof country)
+// console.log(typeof language)
+
+//  LECTURE: let, const and var
+// language = 'german';
+// const country = 'Germany';
+// const continent = 'Europe';
+// const isIsland = true;
+
+// LECTURE: Basic Operators
+// console.log(population / 2);
+// population++
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// const description1 =
+//   country +
+//   " is in " +
+//   continent +
+//   ", and its "
+//   + population +
+//   " million people speak " +
+//   language;
+// console.log(description1);
