0310abc2976ef5908b019ba1043ae5e5b417c4ba
Sebastian Stutz <sebastian.stutz99@gmail.com>
2021-11-05 23:00:22 +0100

    checkpoint after chapter 2

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,37 +1,105 @@
 // LECTURE: Values and Variables
-// let country = "Germany";
-// let continent = "Europe";
-// let population = 82;
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+let country = "Germany";
+let continent = "Europe";
+let population = 82;
+console.log(country);
+console.log(continent);
+console.log(population);
 
 // LECTURE: Data Types
-// let isIsland = false;
-// let language;
-// console.log(typeof isIsland)
-// console.log(typeof population)
-// console.log(typeof country)
-// console.log(typeof language)
+let isIsland = false;
+let language;
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
 
 //  LECTURE: let, const and var
-// language = 'german';
-// const country = 'Germany';
-// const continent = 'Europe';
-// const isIsland = true;
+language = 'german';
 
 // LECTURE: Basic Operators
-// console.log(population / 2);
-// population++
-// console.log(population);
-// console.log(population > 6);
-// console.log(population < 33);
-// const description1 =
-//   country +
-//   " is in " +
-//   continent +
-//   ", and its "
-//   + population +
-//   " million people speak " +
-//   language;
-// console.log(description1);
+console.log(population / 2);
+console.log(population + 1);
+console.log(population > 6);
+console.log(population < 33);
+const description1 =
+  country +
+  " is in " +
+  continent +
+  ", and its "
+  + population +
+  " million people speak " +
+  language;
+console.log(description1);
+
+// LECTURE: Strings and Template Literals
+
+console.log(`${country} is in ${continent}, and its ${population} million people speak ${language}`);
+
+// LECTURE: Taking Decisions: if / else Statements
+
+if (population > 33) {
+  console.log(`${country}'s populaton is above average`);
+} else {
+  console.log(`${country}'s population is ${33 - population} million below average`);
+}
+
+// LECTURE: Type Conversion and Coercion
+
+'9' - '5'; //'4'
+console.log('9' - '5');
+'19' - '13' + '17'; // '617'
+console.log('19' - '13' + '17');
+'19' - '13' + 17; //23
+console.log('19' - '13' + 17);
+'123' < 57; //false
+console.log('123' < 57);
+5 + 6 + '4' + 9 - 4 - 2; //1143
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+// LECTURE: Equality Operators: == vs. ===
+
+// let numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+
+// if (numNeighbours === 1) {
+//   console.log('Only 1 border!');
+// } else if (numNeighbours > 1) {
+//   console.log('More than 1 border');
+// } else {
+//   console.log('No borders');
+// }
+
+// LECTURE: Logical Operators
+
+if (language === 'english' && population < 50 && isIsland === false) {
+  console.log(`You should live in ${country}`)
+} else {
+  console.log(`${country} does not meet your criteria`);
+}
+
+// LECTURE: The switch Statement
+
+switch (language) {
+  case 'chinese':
+  case 'mandarin':
+    console.log('MOST number of native speakers!');
+    break;
+  case 'spanish':
+    console.log('2nd place in number of native speakers');
+    break;
+  case 'english':
+    console.log('3rd place');
+    break;
+  case 'hindi':
+    console.log('Number 4');
+    break;
+  case 'arabic':
+    console.log('5th most spoken language');
+    break;
+  default:
+    console.log('Great language too :D');
+}
+
+// LECTURE: The Conditional (Ternary) Operator
+
+console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average`);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -214,7 +214,6 @@ if (favourite === 23) {
 if (favourite !== 23) {
     console.log('Why not 23?');
 }
-*/
 
 const hasDriversLicense = true; // A
 const hasGoodVision = true; // B
@@ -222,12 +221,11 @@ console.log(hasDriversLicense && hasGoodVision);
 console.log(hasDriversLicense || hasGoodVision);
 console.log(!hasDriversLicense);
 
-
-// if (hasDriversLicense && hasGoodVision) {
-//     console.log('Sarah is able to drive.')
-// } else {
-//     console.log('Someone else should drive...');
-// }
+if (hasDriversLicense && hasGoodVision) {
+    console.log('Sarah is able to drive.')
+ } else {
+     console.log('Someone else should drive...');
+}
 
 const isTired = false; // C
 console.log(hasDriversLicense && hasGoodVision && isTired);
@@ -237,3 +235,64 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 } else {
     console.log('Someone else should drive...');
 }
+
+
+const day = 'friday';
+
+switch (day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else if (day === 'friday') {
+    console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend');
+} else {
+    console.log('Not a valid day!');
+}
+
+const age = 23;
+age >= 18
+    ? console.log('I like to drink wine')
+    : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water'
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+*/
+
